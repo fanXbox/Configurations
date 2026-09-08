cfc089d790c72ed90f56b3fd98e6ba4baa244f4a
fanXbox <107966612+fanXbox@users.noreply.github.com>
2026-09-08 08:58:33 +0200

    Add preheat settings for TPU

--- config/examples/Artillery/Hornet/Configuration.h
+++ config/examples/Artillery/Hornet/Configuration.h
@@ -2573,6 +2573,12 @@
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
+#define PREHEAT_3_LABEL       "TPU"
+#define PREHEAT_3_TEMP_HOTEND 200
+#define PREHEAT_3_TEMP_BED     50
+#define PREHEAT_3_TEMP_CHAMBER 35
+#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
+
 /**
  * @section nozzle park
  *
